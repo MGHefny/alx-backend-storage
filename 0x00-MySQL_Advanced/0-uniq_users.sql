ce082942e424c8dbb2793362b779485a19d22e9c
MGHefny <mgenedhefny@gmail.com>
2024-08-08 19:45:38 +0300

    0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
---user table creat
+--creat user table
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL AUTOINCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
+    PRIMARY KEY (id)
 );
