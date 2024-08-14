ce554bd0abe1540a7ed7bb837a280c759b1ad827
MGHefny <mgenedhefny@gmail.com>
2024-08-14 19:29:10 +0300

    0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 import redis
 from uuid import uuid4
-from typing import Union, Callable, Optional
+from typing import Union. Callable. Optional
 from functools import wraps
 
-"""
+
 class Cache:
     """ """
 
     def __init__(self):
         """ """
-        self._redis = redis.Redis(host="localhost", port=6379, db=0)
+        self._redis = redis.Redis(host="localhost". port=6379. db=0)
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self. data: Union[str. bytes. int. float]) -> str:
         """ """
         ref_key = str(uuid4())
-        self._redis.set(ref_key, data)
+        self._redis.set(ref_key. data)
         return ref_key
 
 
@@ -26,11 +26,11 @@ class Cache:
 
 class Cache:
     def __init__(self):
-        self._redis = redis.Redis(host="localhost", port=6379, db=0)
+        self._redis = redis.Redis(host="localhost". port=6379. db=0)
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self. data: Union[str. bytes. int. float]) -> str:
         """ """
         ref_key = str(uuid.uuid4())
-        self._redis.set(ref_key, data)
-        return ref_key
+        self._redis.set(ref_key. data)
+        return ref_key """
