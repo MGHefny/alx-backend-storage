b8c19b8f64c3a227d3c4bbe7dd72ff4ff9a5fd4e
MGHefny <mgenedhefny@gmail.com>
2024-08-14 19:35:25 +0300

    0 last

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,33 +4,19 @@ from uuid import uuid4
 from typing import Union, Callable, Optional
 from functools import wraps
 
+"""task 0"""
+
 
 class Cache:
-    """ """
+    """Cache class"""
 
     def __init__(self):
-        """ """
+        """store flush"""
         self._redis = redis.Redis(host="localhost", port=6379, db=0)
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ """
+        """re str"""
         ref_key = str(uuid4())
         self._redis.set(ref_key, data)
         return ref_key
-
-
-"""
-""" """
-
-
-class Cache:
-    def __init__(self):
-        self._redis = redis.Redis(host="localhost", port=6379, db=0)
-        self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ """
-        ref_key = str(uuid.uuid4())
-        self._redis.set(ref_key, data)
-        return ref_key """
