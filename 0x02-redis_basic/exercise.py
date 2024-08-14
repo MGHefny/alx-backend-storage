adb41520d866df0b749c71fe50c8f3a3efbdb4df
MGHefny <mgenedhefny@gmail.com>
2024-08-14 19:14:00 +0300

    0test

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""""""
+import redis
+import uuid
+from typing import Union
+
+
+class Cash:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """"""
+        ref_key = str(uuid.uuid4())
+        self._redis.set(ref_key.data)
+        return ref_key
